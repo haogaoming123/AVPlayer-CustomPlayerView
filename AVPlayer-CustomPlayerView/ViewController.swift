4e208bbdd3221726b37e3e8394f3c83d03480aad
郝高明 <978235424@qq.com>
2017-02-15 09:59:09 +0800

    avplayer

---
+++ AVPlayer-CustomPlayerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVPlayer-CustomPlayerView
+//
+//  Created by haogaoming on 2017/2/15.
+//  Copyright © 2017年 郝高明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
