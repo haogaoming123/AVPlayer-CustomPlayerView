ad2223fda0d87370f72a4c70e41084cfdd7408a5
郝高明 <978235424@qq.com>
2017-02-15 10:06:06 +0800

    add pod  snapkit

---
+++ Pods/Target Support Files/Pods-AVPlayer-CustomPlayerView/Pods-AVPlayer-CustomPlayerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AVPlayer_CustomPlayerView : NSObject
+@end
+@implementation PodsDummy_Pods_AVPlayer_CustomPlayerView
+@end
---
+++ Pods/Target Support Files/Pods-AVPlayer-CustomPlayerView/Pods-AVPlayer-CustomPlayerView-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_AVPlayer_CustomPlayerViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AVPlayer_CustomPlayerViewVersionString[];
+
